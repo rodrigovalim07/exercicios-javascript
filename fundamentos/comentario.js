6f06b2c259ae8250f17c84c49df39a9fa0ec202b
Rodrigo Valim <rodrigovalimjr2011@hotmail.com>
2024-06-10 18:17:10 -0300

    Primeiro commit

---
+++ fundamentos/comentario.js
@@ -0,0 +1,16 @@
+// comentário de uma linha
+console.log("linha 1");
+
+/*
+    comentário de
+    múltiplas
+    linas
+*/
+console.log("linha 2");
+
+/*
+ * pode
+ * ser
+ * assim
+ * também
+ */
---
+++ fundamentos/numerosAlgunscuidados.js
@@ -0,0 +1,6 @@
+console.log(7 / 0) // Infinity
+console.log("10" / 2)
+console.log("show" * 2) // NaN - not a number
+console.log(0.1 + 0.7) // 0.799...
+// console.log(10.toString())
+console.log((10.53486).toFixed(2))
