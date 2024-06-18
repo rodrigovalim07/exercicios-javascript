423ffdf92f6515388c63579cbf8033871a9ccddb
Rodrigo Valim <rodrigovalimjr2011@hotmail.com>
2024-06-18 13:43:41 -0300

    Estruturas de controle

---
+++ controle/if2.js
@@ -0,0 +1,19 @@
+function teste1(num) {
+    if (num > 7)
+        console.log(num)
+    
+    console.log('final')
+}
+
+teste1(6)
+teste1(8)
+
+// cuidado com ';'
+function teste2(num) {
+    if (num > 7); {
+        console.log(num)
+    }
+}
+
+teste2(6)
+teste2(8)
