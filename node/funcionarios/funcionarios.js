40bfa4b206573c00ee05c00cb88256a5955ca713
Rodrigo Valim <rodrigovalimjr2011@hotmail.com>
2024-07-15 14:38:16 -0300

    Estudando módulos

---
+++ node/funcionarios/funcionarios.js
@@ -0,0 +1,21 @@
+const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
+const axios = require('axios')
+
+const chineses = func => func.pais === 'China'
+const mulheres = func => func.genero === 'F'
+const menorSalario = (func, funcAtual) => {
+    return func.salario < funcAtual.salario ? func : funcAtual
+}
+
+axios.get(url).then(response => {
+    const funcionarios = response.data
+    console.log(funcionarios)
+
+    // mulher chinesa com menor salário
+    const func = funcionarios
+        .filter(chineses)
+        .filter(mulheres)
+        .reduce(menorSalario)
+
+    console.log(func)
+})
